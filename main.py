55b7014543e53b459d404fa0f5c5b77d2b15c5ad
Nick Jantz <thenickjantz@gmail.com>
2020-12-13 10:46:39 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+# Main file run on micropython startup
+# Used to download the /src folder from this repo and run robot instructions
+# A faux CI/CD platform so once the robot is initialized it will always have the latest updates
+
